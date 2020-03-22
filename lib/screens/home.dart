85ff82d403390ca758156c9ee98b72418e08a604
johnnylyimo <johnnylyimo@gmail.com>
2020-03-23 02:49:05 +0300

    Defined AppBar centerTitle

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,7 +9,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+      ),
     );
   }
 }
