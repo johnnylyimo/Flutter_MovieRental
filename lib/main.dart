d95ddee155ec67cda316b78ac5e8de751d61b581
johnnylyimo <johnnylyimo@gmail.com>
2020-03-23 02:39:02 +0300

    Added Project Starter files

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MovieRental());
+}
+
+class MovieRental extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MovieRental App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
