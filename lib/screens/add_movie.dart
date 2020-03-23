7a8b1331a7390ca1fb13474fd2ba7b202c1e38d4
johnnylyimo <johnnylyimo@gmail.com>
2020-03-23 17:10:47 +0300

    Defined TextFormField 2 InputDecoration enabledBorder

--- lib/screens/add_movie.dart
+++ lib/screens/add_movie.dart
@@ -54,6 +54,13 @@ class _AddMovieState extends State<AddMovie> {
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20.0),
                   ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue.shade200,
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
                 validator: (val) =>
                     val.isEmpty ? 'Please enter Movie Title' : null,
