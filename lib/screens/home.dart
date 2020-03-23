ef31f749f503ec004e839f9b82ecf9fe0b74f091
johnnylyimo <johnnylyimo@gmail.com>
2020-03-23 16:25:30 +0300

    Imported DBManager package

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:movierental/model/movie.dart';
+import 'package:movierental/model/db_manager.dart';
 
 class HomePage extends StatefulWidget {
   @override
