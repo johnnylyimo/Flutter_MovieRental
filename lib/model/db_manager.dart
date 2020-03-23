6043e7a785221a01918e607c34ea49510f71a07d
johnnylyimo <johnnylyimo@gmail.com>
2020-03-23 14:39:20 +0300

    Created DBManager class instance

--- lib/model/db_manager.dart
+++ lib/model/db_manager.dart
@@ -4,4 +4,5 @@ import 'package:flutter/foundation.dart';
 class DBManager with ChangeNotifier {
   // Transform DBManager to singleton class.
   DBManager._internal();
+  static final DBManager _dbManager = DBManager._internal();
 }
