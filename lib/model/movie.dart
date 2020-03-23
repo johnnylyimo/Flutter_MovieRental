e8316fb8517200fbbad19b798e3e1e607e3ba713
johnnylyimo <johnnylyimo@gmail.com>
2020-03-23 14:11:17 +0300

    Defined setter releasedYear

--- lib/model/movie.dart
+++ lib/model/movie.dart
@@ -4,7 +4,7 @@ class Movie {
   String _title;
   String _actor;
   String _desc;
-  String _releasedYear;
+  int _releasedYear;
 
   // Constructor
   Movie(this._id, this._title, this._actor, this._releasedYear, [this._desc]);
@@ -14,7 +14,7 @@ class Movie {
   String get title => _title;
   String get actor => _actor;
   String get description => _desc;
-  String get releasedYear => _releasedYear;
+  int get releasedYear => _releasedYear;
 
   // setters
   set title(String newTitle) {
@@ -34,4 +34,6 @@ class Movie {
       _desc = newDescription;
     }
   }
+
+  set releasedYear(int newReleasedYear) => _releasedYear = newReleasedYear;
 }
