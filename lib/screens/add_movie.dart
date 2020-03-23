d46a440d6cca2c49a120fcdb2fdcfcdcf0dbd8a8
johnnylyimo <johnnylyimo@gmail.com>
2020-03-23 16:34:44 +0300

    Created AddMovie Stateful widget

--- lib/screens/add_movie.dart
+++ lib/screens/add_movie.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class AddMovie extends StatefulWidget {
+  @override
+  _AddMovieState createState() => _AddMovieState();
+}
+
+class _AddMovieState extends State<AddMovie> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
