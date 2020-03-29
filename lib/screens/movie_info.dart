7050907fedbf5a8f5c39591b67f5f1c39d1463f1
johnnylyimo <johnnylyimo@gmail.com>
2020-03-29 15:00:17 +0300

    Created MovieInfo Stateful widget

--- lib/screens/movie_info.dart
+++ lib/screens/movie_info.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class MovieInfo extends StatefulWidget {
+  @override
+  _MovieInfoState createState() => _MovieInfoState();
+}
+
+class _MovieInfoState extends State<MovieInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
