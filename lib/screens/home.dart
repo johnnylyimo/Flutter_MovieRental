46867fe92c4c5983cce388233797ba13667d6235
johnnylyimo <johnnylyimo@gmail.com>
2020-03-29 14:54:31 +0300

    Defined Card elevation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -35,6 +35,7 @@ class _HomePageState extends State<HomePage> {
             itemCount: movies.length ?? 0,
             itemBuilder: (context, index) {
               return Card(
+                elevation: 6.0,
                 shape: StadiumBorder(),
                 child: ListTile(
                   leading: Icon(Icons.movie),
