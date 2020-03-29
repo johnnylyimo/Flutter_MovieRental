6f3ea2535ac5bce37b59e2d6cac5adc04a9d823b
johnnylyimo <johnnylyimo@gmail.com>
2020-03-29 15:53:42 +0300

    Added SizeBox2 to the Column children

--- lib/screens/movie_info.dart
+++ lib/screens/movie_info.dart
@@ -79,6 +79,7 @@ class _MovieInfoState extends State<MovieInfo> {
                     validator: (val) =>
                         val.isEmpty ? 'Please enter Movie Actor(s)' : null,
                   ),
+                  SizedBox(),
                 ],
               ),
             ),
