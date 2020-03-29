37d5b257c40f5d57d9ccefce6d8b3e5da1e2c67c
johnnylyimo <johnnylyimo@gmail.com>
2020-03-29 13:34:32 +0300

    Added floatingActionButton onPressed method

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -33,6 +33,7 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
