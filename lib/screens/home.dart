aee3953c9417f88a412ba6c7d62d6c0c8f76782f
johnnylyimo <johnnylyimo@gmail.com>
2020-03-30 19:02:34 +0300

    Returned Center widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:movierental/model/movie.dart';
 import 'package:movierental/model/db_manager.dart';
-import 'dart:async';
+import 'package:movierental/model/movie.dart';
 import 'package:movierental/screens/add_movie.dart';
 import 'package:movierental/screens/movie_info.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -50,29 +49,33 @@ class _HomePageState extends State<HomePage> {
         child: Container(
           child: FutureBuilder(
             future: getDB.getMovies(),
-            builder: (context, snapshot),
-            return ListView.builder(
-              itemCount: movies.length ?? 0,
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 6.0,
-                  shape: StadiumBorder(),
-                  child: ListTile(
-                    leading: Icon(Icons.movie),
-                    title: Text(movies[index].title),
-                    subtitle: Text((movies[index].actor)),
-                    trailing: Text((movies[index].releasedYear.toString())),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MovieInfo(movies[index])),
-                      );
-                    },
-                  ),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: movies.length ?? 0,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 6.0,
+                      shape: StadiumBorder(),
+                      child: ListTile(
+                        leading: Icon(Icons.movie),
+                        title: Text(movies[index].title),
+                        subtitle: Text((movies[index].actor)),
+                        trailing: Text((movies[index].releasedYear.toString())),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MovieInfo(movies[index])),
+                          );
+                        },
+                      ),
+                    );
+                  },
                 );
-              },
-            ),
+              }
+              return Center();
+            },
           ),
         ),
       ),
