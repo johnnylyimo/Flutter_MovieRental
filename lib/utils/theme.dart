21d4aa5aa38e0ec5db5a58ce84b794073e4656b7
johnnylyimo <johnnylyimo@gmail.com>
2020-05-03 15:09:27 +0300

    Defined scaffoldBackgroundColor

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MyTheme {
-  ThemeData buildTheme() {}
+  ThemeData buildTheme() {
+    return ThemeData(
+      scaffoldBackgroundColor: Colors.blue.shade100;
+    );
+  }
 }
